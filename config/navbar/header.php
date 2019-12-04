9fe9f94945516da962829da442e33e84f5dd54d5
Ida Lam <idaslam@outlook.com>
2019-12-04 21:13:03 +0100

    added blogg in navbar

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -40,6 +40,11 @@ return [
                         "url" => "redovisning/kmom04",
                         "title" => "Redovisning för kmom04.",
                     ],
+                    [
+                        "text" => "Kmom05",
+                        "url" => "redovisning/kmom05",
+                        "title" => "Redovisning för kmom05.",
+                    ],
                 ],
             ],
         ],
@@ -61,6 +66,11 @@ return [
         [
             "text" => "Test",
             "url" => "test",
+            "title" => "Testa olika saker med markdown",
+        ],
+        [
+            "text" => "Blogg",
+            "url" => "htdocs/blogg",
             "title" => "Testa olika saker med markdown",
         ],
     ],
